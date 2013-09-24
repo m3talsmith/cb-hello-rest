75402be8a76fc3c302b7cf48f58b893732a44253
Michael Christenson II <mchristenson@revspringinc.com>
2013-09-24 13:25:53 -0700

    broke through the rest barrier

--- src/controller/cbhellorest_greetings_controller.erl
+++ src/controller/cbhellorest_greetings_controller.erl
@@ -1,9 +1,14 @@
 -module(cbhellorest_greetings_controller, [Req]).
 -compile(export_all).
+-default_action(index).
+
+index('GET', []) ->
+  Greetings = boss_db:find(greeting, []),
+  {ok, [{greetings, Greetings}]}.
 
 hello('GET', []) ->
   {json, [{message, "hey"}]};
 hello('POST', []) ->
   NewGreeting = greeting:new(id, Req:post_params("message")),
-  {ok, SavedGreeting} = NewGreeting.save(),
-  {json, [{message, SavedGreeting:attributes}]}.
+  {ok, SavedGreeting} = NewGreeting:save(),
+  {json, [{message, SavedGreeting:attributes()}]}.
