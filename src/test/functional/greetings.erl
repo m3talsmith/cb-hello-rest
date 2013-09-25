1b383f3470313c53d9c04b2fe1c2ef462bbaf0b2
Michael Christenson II <mchristenson@revspringinc.com>
2013-09-25 11:44:45 -0700

    Sort of functioning request test - fails in a weird way

--- src/test/functional/greetings.erl
+++ src/test/functional/greetings.erl
@@ -1,9 +1,9 @@
 -module(cbhellorest_test).
--export([start/1]).
+-export([start/0]).
 
 start() ->
   boss_web_request:get_request('/greetings/', [],
     [% Making sure the index comes up and that we have a link to add a greeting
       fun boss_assert:http_ok/1,
-      fun(Res) -> boss_assert:link_with_text('Greet us!', Res)
+      fun(Res) -> boss_assert:link_with_text('Greet us!', Res) end
     ], []).
