17953d0356153435ce4cfb9805b88e88c0c80a54
Michael Christenson II <michael@deepwoodsbrigade.com>
2013-09-25 17:53:33 -0700

    added application:start call - attempting to spin up boss

--- src/test/functional/greetings.erl
+++ src/test/functional/greetings.erl
@@ -2,6 +2,7 @@
 -export([start/0]).
 
 start() ->
+  application:start(boss),
   boss_web_request:get_request('/greetings/', [],
     [% Making sure the index comes up and that we have a link to add a greeting
       fun boss_assert:http_ok/1,
